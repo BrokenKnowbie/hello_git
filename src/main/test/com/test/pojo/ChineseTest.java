7124601ca8b42cd7440ef1dc41e01249360ab7d2
v_vbeiliu <1370316163@qq.com>
2019-05-07 11:17:04 +0800

    update new code

--- src/main/test/com/test/pojo/ChineseTest.java
+++ src/main/test/com/test/pojo/ChineseTest.java
@@ -1,25 +1,34 @@
 package com.test.pojo;
 
-import com.test.DAO.Plant;
 import org.junit.Test;
+
+import com.test.DAO.Plant;
 
 public class ChineseTest {
     Plant apple = new Apple(2);
     Plant pear = new Pear(3);
     Chinese zhang = new Chinese("张三");
-    Chinese li    = new Chinese("李四");
+    American li    = new American("斯坦李");
 
     @Test
     public void attack() {
         zhang.showInfo();
         li.showInfo();
+        System.out.println("-----分割线------------------");
         zhang.attack(li);
+        zhang.showInfo();
+        li.showInfo();
+        System.out.println("-----分割线------------------");
+        pear.showInfo();
         li.eat(pear);
-        zhang.showInfo();
         li.showInfo();
-        li.attack(zhang);
-        li.showInfo();
-        zhang.showInfo();
+        System.out.println("-----分割线------------------");
+        while (li.getHEALTH()>0){
+            zhang.attack(li);
+            zhang.showInfo();
+            li.showInfo();
+        }
+
     }
 
     @Test
